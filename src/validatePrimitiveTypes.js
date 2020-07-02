a33a76b275659b97ca818a5bfcfb2966dc5ba0da
DG-Wangtao <taowang@deepglint.com>
2020-07-02 14:11:30 +0800

    feat: 添加中国二代身份证号码校验

--- src/validatePrimitiveTypes.js
+++ src/validatePrimitiveTypes.js
@@ -67,6 +67,7 @@ exports.validateNumber = validateNumber;
 /**
  * validateBoolean
  * @param {boolean} candidate - value
+ * @param {*} dataType - value
  * @return {Error}
  */
 function validateBoolean(candidate, dataType) {
@@ -99,6 +100,60 @@ function validateFile() {
   // Not sure how to check this, since anything could qualify as 'File'.
 }
 exports.validateFile = validateFile;
+
+
+/**
+ * 身份证校验算法
+ * @param {string} idcode 身份证号码
+ * @return {bool}
+ */
+function isCNIdentityCard(idcode) {
+  if ( typeof value != 'string' || value.length != 18 ) {
+    return false;
+  }
+  // 加权因子
+  const weightFactor = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+  // 校验码
+  const checkCode = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'];
+
+  const code = idcode + '';
+  const last = idcode[17]; // 最后一位
+
+  const seventeen = code.substring(0, 17);
+
+  // ISO 7064:1983.MOD 11-2
+  // 判断最后一位校验码是否正确
+  const arr = seventeen.split('');
+  const len = arr.length;
+  let num = 0;
+  for (let i = 0; i < len; i++) {
+    num = num + parseInt(arr[i]) * weightFactor[i];
+  }
+
+  // 获取余数
+  const resisue = num % 11;
+  const lastNo = checkCode[resisue];
+
+  // 格式的正则
+  // 正则思路
+  /*
+    第一位不可能是0
+    第二位到第六位可以是0-9
+    第七位到第十位是年份，所以七八位为19或者20
+    十一位和十二位是月份，这两位是01-12之间的数值
+    十三位和十四位是日期，是从01-31之间的数值
+    十五，十六，十七都是数字0-9
+    十八位可能是数字0-9，也可能是X
+    */
+  const patter = /^[1-9][0-9]{5}([1][9][0-9]{2}|[2][0][0|1][0-9])([0][1-9]|[1][0|1|2])([0][1-9]|[1|2][0-9]|[3][0|1])[0-9]{3}([0-9]|[X])$/;
+
+  // 判断格式是否正确
+  const format = patter.test(idcode);
+
+  // 返回验证结果，校验码和格式同时正确才算是合法的身份证号码
+  return last === lastNo && format ? true : false;
+}
+
 
 /**
  * validateString
@@ -163,6 +218,8 @@ function validateString(candidate, dataType, format, pattern) {
       case 'password':
         pattern= PASSWORD_RE;
         break;
+      case 'CNIdentityCard':
+        return isCNIdentityCard(candidate);
     }
   }
 
@@ -179,7 +236,7 @@ function validateString(candidate, dataType, format, pattern) {
  * chack if candidate is null dataType['nullable'] is default to be false
  * @param {*} candidate - value
  * @param {*} dataType - datatype
- * @return
+ * @return {bool}
  */
 function validateNull(candidate, dataType) {
   if (candidate === null) {
